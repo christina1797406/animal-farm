0dae53cc6102c3bd52d70df1a7e4ff1611445c53
Samira <a1852922@adelaide.edu.au>
2024-05-12 16:16:36 +0930

    final

--- Equipment.cpp
+++ Equipment.cpp
@@ -1,14 +1,11 @@
 #include "Equipment.h"
 
-// Constructor
 Equipment::Equipment() : no_of_equipment(0) {}
 
 Equipment::Equipment(int no_of_equipment) : no_of_equipment(no_of_equipment) {}
 
-// Setter method for number of equipment
 void Equipment::set_no_of_equipment(int count) {
   this->no_of_equipment = count;
 }
 
-// Getter method for numbe rof equipment
 int Equipment::get_no_of_equipment() const { return no_of_equipment; }
--- main-equipment.cpp
+++ main-equipment.cpp
@@ -1,3 +1,6 @@
+// main.cpp
+#include <iostream>
+
 #include "Shovel.h"
 #include "WaterCan.h"
 
@@ -7,8 +10,14 @@ int main() {
   Shovel shovel;
 
   // Set the number of equipment for each instance
-  waterCan.set_no_of_equipment(3);
-  shovel.set_no_of_equipment(2);
+  waterCan.set_no_of_equipment(1);
+  shovel.set_no_of_equipment(1);
+
+  // Display the number of equipment
+  std::cout << "Number of Water Cans: " << waterCan.get_no_of_equipment()
+            << std::endl;
+  std::cout << "Number of Shovels: " << shovel.get_no_of_equipment()
+            << std::endl;
 
   // Use the equipment (virtual function) for each instance
   waterCan.Use();  // This will call the overridden function in WaterCan
