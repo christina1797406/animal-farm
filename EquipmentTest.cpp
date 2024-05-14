e21c64186f86a22c8bab6ffbfe57c44b8a09e943
Samira <a1852922@adelaide.edu.au>
2024-05-14 18:41:11 +0930

    final

---
+++ EquipmentTest.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "Shovel.h"
+#include "WaterCan.h"
+
+class EquipmentTest {
+ public:
+  void runTests() {
+    testSetNoOfEquipment();
+    testGetNoOfEquipment();
+  }
+
+ private:
+  void testSetNoOfEquipment() {
+    WaterCan waterCan;  // Creating an instance of WaterCan instead of Equipment
+    waterCan.set_no_of_equipment(5);
+    if (waterCan.get_no_of_equipment() != 5) {
+      std::cout << "Test set_no_of_equipment failed!" << std::endl;
+    }
+  }
+
+  void testGetNoOfEquipment() {
+    Shovel shovel;  // Creating an instance of Shovel instead of Equipment
+    shovel.set_no_of_equipment(10);
+    if (shovel.get_no_of_equipment() != 10) {
+      std::cout << "Test get_no_of_equipment failed!" << std::endl;
+    }
+  }
+};
+
+int main() {
+  EquipmentTest equipTest;
+  equipTest.runTests();
+
+  return 0;
+}
---
+++ seasonalVegTest.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "seasonalVeg.h"
+
+class SeasonalVegTest {
+ public:
+  void runTests() {
+    testSetSeason();
+    testGetSeason();
+  }
+
+ private:
+  void testSetSeason() {
+    seasonalVeg veg;
+    veg.setSeason("Spring");
+    if (veg.getSeason() != "Spring") {
+      std::cout << "Test setSeason failed!" << std::endl;
+    }
+  }
+
+  void testGetSeason() {
+    seasonalVeg veg("Potato", 5, 3, 2.5, "Potato", 15, "Summer");
+    if (veg.getSeason() != "Summer") {
+      std::cout << "Test getSeason failed!" << std::endl;
+    }
+  }
+};
+
+int main() {
+  SeasonalVegTest vegTest;
+  vegTest.runTests();
+
+  return 0;
+}
