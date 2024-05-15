3d1f3776739b6d61192e4777d02bcd57f4c61216
Christina Nguyen <christina@Christinas-MacBook-Pro-3.local>
2024-05-15 19:15:42 +0930

    adding background and user character

--- GameEntity.h
+++ GameEntity.h
@@ -17,16 +17,27 @@ class GameEntity {
         User* user;
         std::vector<Flower*> flowers;       // to store multiple flowers
 
+        sf::Texture backgroundTexture;
+        sf::Sprite backgroundSprite;
+
         sf::Text info;
         sf::Font font;
 
 
     public:
-        GameEntity(int size, std::string title) {
-            win =       new sf::RenderWindow(sf::VideoMode(size,size), title);
-            user =      new User();
+        GameEntity(int x_size, int y_size, std::string title) {
+            win =       new sf::RenderWindow(sf::VideoMode(x_size, y_size), title);
+            user =      new User("Christina", 30);
 
             //// add code
+            if (!backgroundTexture.loadFromFile("./sfml/background.png")) {
+                std::cout << "Failed to load background file." << std::endl;
+                return;
+            }
+
+            backgroundSprite.setTexture(backgroundTexture);
+            backgroundSprite.setPosition(0.0f, 0.0f);
+            backgroundSprite.setScale(0.5f, 0.5f);
         }
     
 
@@ -42,12 +53,29 @@ class GameEntity {
 
                     // add more functions for windows
 
+                    // 1. to move player
+                    if (Keyboard::isKeyPressed(Keyboard::A)) {
+                            user->move_left(6);       
+                    } else if (Keyboard::isKeyPressed(Keyboard::D)) {
+                            user->move_right(6);
+                    } else if (Keyboard::isKeyPressed(Keyboard::W)) {
+                            user->move_up(6);
+                    } else if (Keyboard::isKeyPressed(Keyboard::S)) {
+                            user->move_down(6);
+                    }
+
+                    
 
 
                     
                 }
 
                 win->clear();
+                win->draw(backgroundSprite);
+
+                user->draw(win);
+                //flower->draw(*win);
+
                 win->display();
                 
             }
--- User.h
+++ User.h
@@ -2,10 +2,13 @@
 #define USER_H
 #include <string>
 
+#include <SFML/Graphics.hpp>
+
 class User {
  public:
-  User();  // constructor
+  User();                           // constructor
   User(std::string name, int age);
+  
   // Setter methods
   void setName(std::string name);
   void setAge(int age);
@@ -14,9 +17,32 @@ class User {
   std::string getName() const;
   int getAge() const;
 
+  // Draw player
+  void draw(sf::RenderWindow* window);
+
+  // Move the player
+  void move_right(int speed) {      // speed is x, 0 is y
+      sprite.move(speed,0);
+  }
+  void move_left(int speed) {
+      sprite.move(-speed,0);
+  }
+  void move_down(int speed) {
+      sprite.move(0,speed);
+  }
+  void move_up(int speed) {
+      sprite.move(0,-speed);
+  }
+
  private:
   std::string name;  // user's name
   int age;           // user's age
+  
+  sf::Texture texture;
+  sf::Sprite sprite;
+
+  int x = 5;
+  int y = 5;
 };
 
 #endif
